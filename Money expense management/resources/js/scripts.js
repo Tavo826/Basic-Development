b3be2228670f479c25dda9ccbfac1df08a6c81f5
Tavo826 <gustavo.orozcom68@gmail.com>
2021-02-22 00:32:43 -0500

    .

--- Money expense management/resources/js/scripts.js
+++ Money expense management/resources/js/scripts.js
@@ -1,46 +1,46 @@
-/*Capturando el evento submit (envío del formulario)*/
+//Capturando el evento submit (envío del formulario)
 const form = document.getElementById('transactionForm')
-/*console.log(form)*/
-/*Detectando el evento submit (cuando se presiona agregar)*/
+//console.log(form)
+//Detectando el evento submit (cuando se presiona agregar)
 form.addEventListener('submit', function(event){
-        /*console.log(event);*/
-        /*alert('Se detecto un envio del formulario')*/
+        //console.log(event);
+        //alert('Se detecto un envio del formulario')
         
-        /*Evitando que se recargue la página con el evento*/
-        /*y que se envíe el formulario al servidor*/
+        //Evitando que se recargue la página con el evento
+        //y que se envíe el formulario al servidor
         event.preventDefault();
-        /*Obteniendo los datos del formulario*/
+        //Obteniendo los datos del formulario
         let transactionFormData = new FormData(form)
-        /*Convirtiendo los datos del FormData en un objeto*/
+        //Convirtiendo los datos del FormData en un objeto
         let transactionObj = convertFormDataToTransactionObj(transactionFormData)
-        /*Guardando los datos en el Local Storage*/
+        //Guardando los datos en el Local Storage
         saveTransactionObj(transactionObj)
-        /*Insertando la fila en la tabla*/
+        //Insertando la fila en la tabla
         insertRowInTransactionTable(transactionObj)
 
-        /*Borrando los datos del formulario que ya se agregaron a la tabla*/
+        //Borrando los datos del formulario que ya se agregaron a la tabla
         form.reset()
     }
 )
 
-/*Detectando el evento DOMContentLoaded (cuando el documento html ha sido completamente cargado y parseado)*/
+//Detectando el evento DOMContentLoaded (cuando el documento html ha sido completamente cargado y parseado)
 document.addEventListener('DOMContentLoaded', function(event){
-        /*Convirtiendo en array el objeto en el Local storage*/
+        //Convirtiendo en array el objeto en el Local storage
         let transactionObjArr = JSON.parse(localStorage.getItem('transactionData')) || []
-        /*Se recorre cada elemento del array*/
+        //Se recorre cada elemento del array
         transactionObjArr.forEach(function(arrElement){
-            /*Se inserta en la tabla la fila actual*/
+            //Se inserta en la tabla la fila actual
             insertRowInTransactionTable(arrElement)
         });
     }
 )
 
 function getNewTransactionId(){
-    /*Obteniendo el Id desde el local storage, si es vacío se le asigna -1*/
+    //Obteniendo el Id desde el local storage, si es vacío se le asigna -1
     let lastTransactionId = localStorage.getItem('lastTransactionId') || '-1'
-    /*Sumando 1 al Id*/
+    //Sumando 1 al Id
     let newTransactionId = JSON.parse(lastTransactionId) + 1
-    /*Guardando en el local storage*/
+    //Guardando en el local storage
     localStorage.setItem('lastTransactionId', JSON.stringify(newTransactionId))
     
     return newTransactionId
@@ -64,12 +64,12 @@ function convertFormDataToTransactionObj(transactionFormData){
 
 function insertRowInTransactionTable(transactionObj){
 
-    /*Obteniendo referencia de la tabla*/
+    //Obteniendo referencia de la tabla
     let transactionTableRef = document.getElementById('transactionTable')
-    /*Insertando nueva fila a la tabla*/
+    //Insertando nueva fila a la tabla
     let newTransactionRowRef = transactionTableRef.insertRow(-1)
     newTransactionRowRef.setAttribute('data-transaction-id', transactionObj['transactionId'])
-    /*Insertando las celdas en esa fila*/
+    //Insertando las celdas en esa fila
     let newTypeCellRef = newTransactionRowRef.insertCell(0)
     newTypeCellRef.textContent = transactionObj['transactionType']
 
@@ -83,46 +83,46 @@ function insertRowInTransactionTable(transactionObj){
     newTypeCellRef.textContent = transactionObj['transactionDescription']
 
     let newDeleteCell = newTransactionRowRef.insertCell(4)
-    /*Creando el botón*/
+    //Creando el botón
     let deleteButton = document.createElement('button')
-    /*Dándole un texto al botón*/
+    //Dándole un texto al botón
     deleteButton.textContent = 'Eliminar'
-    /*Agregando el botón a la celda*/
+    //Agregando el botón a la celda
     newDeleteCell.appendChild(deleteButton)
 
-    /*Capturando el evento del botón*/
+    //Capturando el evento del botón
     deleteButton.addEventListener('click', () => {
-        /*Accediendo al elemento (botón), luego al td (columna) y luego al tr (fila)*/
+        //Accediendo al elemento (botón), luego al td (columna) y luego al tr (fila)
         let transactionRow = event.target.parentNode.parentNode
-        /*Obteniendo el id de esa fila*/
+        //Obteniendo el id de esa fila
         let transactionId = transactionRow.getAttribute('data-transaction-id')
-        /*Removiendo la fila del html*/
+        //Removiendo la fila del html
         transactionRow.remove()
-        /*Removiendo la fila del local storage*/
+        //Removiendo la fila del local storage
         deleteTransactionObj(transactionId)
     })
 }
 
 function deleteTransactionObj(transactionId){
-    /*Obteniendo todas las transacciones*/
+    //Obteniendo todas las transacciones
     let transactionObjArr = JSON.parse(localStorage.getItem('transactionData'))
-    /*Se busca el índice que se desea eliminar del array*/
+    //Se busca el índice que se desea eliminar del array
     let transactionIndexInArr = transactionObjArr.findIndex(element => element.transactionId === transactionId)
-    /*Eliminando el elemento del array*/
+    //Eliminando el elemento del array
     transactionObjArr.splice(transactionIndexInArr, 1)
-    /*Convirtiendo el array de transacción a json*/
+    //Convirtiendo el array de transacción a json
     let transactionArrJSON = JSON.stringify(transactionObjArr)
-    /*Se guarda nuevamente el array en el locar storage*/
+    //Se guarda nuevamente el array en el locar storage
     localStorage.setItem('transactionData', transactionArrJSON)
 }
 
 function saveTransactionObj(transactionObj){
-    /*Revisando si en el Local Storage hay un array y cargándolo*/
+    //Revisando si en el Local Storage hay un array y cargándolo
     let myTransactionArr = JSON.parse(localStorage.getItem('transactionData')) || []
-    /*Añadiendo la transacción a un array*/
+    //Añadiendo la transacción a un array
     myTransactionArr.push(transactionObj)
-    /*Convirtiendo el array de transacción a json*/
+    //Convirtiendo el array de transacción a json
     let transactionArrJSON = JSON.stringify(myTransactionArr)
-    /*Guardando el array en el Local Store*/
+    //Guardando el array en el Local Store
     localStorage.setItem('transactionData', transactionArrJSON)
 }
