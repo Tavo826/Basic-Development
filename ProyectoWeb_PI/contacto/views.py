aa940120516d9ea903bbd191d90bd62ef5336813
Tavo826 <gustavo.orozcom68@gmail.com>
2021-03-31 15:01:49 -0500

    .

--- ProyectoWeb_PI/contacto/views.py
+++ ProyectoWeb_PI/contacto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 #importando el formulario
 from .forms import FormularioContacto
 
@@ -7,6 +7,21 @@ from .forms import FormularioContacto
 def contacto(request):
     #instanciando el formulario
     formulario_contacto = FormularioContacto()
+
+    if request.method == 'POST':
+        #Se carga en el formulario la info introducida
+        formulario_contacto = FormularioContacto(data=request.POST)
+        #Si el formulario es válido
+        if formulario_contacto.is_valid():
+            #Se guardan los datos
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+            
+            #Aprovechando la recarga de la página al presionar enviar
+            #Se redirecciona a la url de contacto pasando un parámetro
+
+            return redirect('/contacto/?valido')
 
     #return HttpResponse('Contacto')
     return render(request, 'contacto/contacto.html', {'miFormulario': formulario_contacto})
